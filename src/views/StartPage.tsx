b7f3a86811333c02b61bf99611738bbc619a8a95
DaoZhang1588 <1205308740@qq.com>
2022-09-22 20:32:23 +0800

    完成导航栏

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -2,6 +2,10 @@ import { defineComponent} from "vue";
 import {Button} from "../shared/Button";
 import s from "./StartPage.module.scss"
 import {FloatButton} from "../shared/FloatButton";
+import {Center} from "../shared/Center";
+import pig from '../assets/icons/pig.svg'
+import {Navbar} from "../shared/Navbar";
+import menu from '../assets/icons/menu.svg'
 export const StartPage =defineComponent({
 
 setup(){
@@ -10,8 +14,16 @@ setup(){
     }
 return()=>(
     <div>
+        <nav>
+            <Navbar>{
+                {default:'山竹记账',icon:<img src={menu} alt={menu} class={s.navIcon}/>}
+            }</Navbar>
+        </nav>
+        <Center class={s.pig_wrapper}>
+            <img src={pig} alt={pig} class={s.pig}/>
+        </Center>
     <div class={s.button_wrapper} >
-     <Button class={s.button} onClick={onClick}>测试</Button>
+     <Button class={s.button} onClick={onClick}>开始记账</Button>
     </div>
         <FloatButton/>
     </div>
