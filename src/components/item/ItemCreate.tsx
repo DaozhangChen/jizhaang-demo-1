a22cd84d0fc748ad8f540bf102c932a9dee582d1
DaoZhang1588 <1205308740@qq.com>
2022-09-23 16:11:25 +0800

    完成Tabs组件的

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,7 +1,8 @@
-import {defineComponent, PropType} from "vue";
+import {defineComponent, PropType, ref} from "vue";
 import s from './ItemCreate.module.scss'
 import comeback from "../../assets/icons/comeback.svg";
 import {MainLayout} from "../../layouts/MainLayout";
+import {Tab, Tabs} from "../../shared/Tabs";
 
 export const ItemCreate = defineComponent({
     props:{
@@ -10,14 +11,24 @@ export const ItemCreate = defineComponent({
         }
     },
     setup:(props,context)=>{
+        const refKind=ref('支出')
+        const onUpdateSelected=(name:string)=> refKind.value=name
         return ()=>(
             <MainLayout>{
                 {
                     title:()=>'记一笔',
                     icon:()=><img src={comeback} class={s.navIcon}/>,
-                    default:()=><>
-                        <div>main</div>
-                    </>
+                    default:()=>
+                        <>
+                        <Tabs v-model:selected={refKind.value}>
+                        <Tab name='支出'>
+                            icon 列表
+                        </Tab>
+                        <Tab name='收入'>
+                            icon 列表2
+                        </Tab>
+                        </Tabs>
+                        </>
                 }
             }</MainLayout>
         )
