5eebaa2f7c67f885468109e25cc9c90b8864efc5
DaoZhang1588 <1205308740@qq.com>
2022-09-28 20:16:20 +0800

    完成eCharts的引入

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,26 +1,32 @@
-import {defineComponent, PropType, ref} from "vue";
-import s from './Charts.module.scss'
-import {FormItem} from "../../shared/From";
+import { defineComponent, PropType, ref } from 'vue';
+import { FormItem} from "../../shared/From";
+import s from './Charts.module.scss';
+import { LineChart } from './LineChart';
+import { PieChart } from './PieChart';
+import { Bars } from './Bars';
 
-export const Charts =defineComponent({
-    props:{
-        startDate:{
-            type:String as PropType<string>,
-            required:true
+export const Charts = defineComponent({
+    props: {
+        startDate: {
+            type: String as PropType<string>,
+            required: true
         },
-        endDate:{
-            type:String as PropType<string>,
+        endDate: {
+            type: String as PropType<string>,
             required: true
         }
     },
-    setup:(props,context)=>{
-        const category=ref('expenses')
-        return()=>(
+    setup: (props, context) => {
+        const category = ref('expenses')
+        return () => (
             <div class={s.wrapper}>
-                <FormItem label='类型' type='select' options={[
-                    {value:'expenses',text:'支出'},
-                    {value:'income',text:'收入'}
-                ]} v-model={category.value}/>
+                <FormItem label='类型' type="select" options={[
+                    { value: 'expenses', text: '支出' },
+                    { value: 'income', text: '收入' }
+                ]} v-model={category.value} />
+                <LineChart />
+                <PieChart />
+                <Bars />
             </div>
         )
     }
