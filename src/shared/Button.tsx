c0602073ddce4fa005ca2524a51a24f35d7c9727
DaoZhang1588 <1205308740@qq.com>
2022-10-04 16:01:14 +0800

    重构开始动画按钮

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -6,7 +6,7 @@ interface Props{
 export const Button = defineComponent({
     props:{
         onClick:{
-            type:Function as PropType<(e:MouseEvent)=>void>
+            type:Function as PropType<()=>void>
         },
         level:{
             type:String as PropType<'important'|'normal'|'danger'>,
