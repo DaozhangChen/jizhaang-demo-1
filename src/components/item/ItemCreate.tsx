246520b1f0e2df02c8d649e7a8b890f8bb5d2d01
DaoZhang1588 <1205308740@qq.com>
2022-10-05 19:19:50 +0800

    封装Tags组件

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,104 +1,50 @@
-import {defineComponent, onMounted, PropType, ref} from "vue";
-import s from './ItemCreate.module.scss'
+import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { http } from '../../shared/Http';
+import { Tabs, Tab } from '../../shared/Tabs';
+import { useTags } from '../../shared/useTags';
+import { InputPad } from './InputPad';
+import s from './ItemCreate.module.scss';
+import { Tags } from './Tags';
 import comeback from "../../assets/icons/comeback.svg";
-import {MainLayout} from "../../layouts/MainLayout";
-import {Tab, Tabs} from "../../shared/Tabs";
-import {InputPad} from "./InputPad";
-import addBlack from '../../assets/icons/addblack.svg'
-import {http} from "../../shared/Http";
-import {Button} from "vant";
-
 export const ItemCreate = defineComponent({
-    props:{
-        name:{
-            type:String as PropType<string>
+    props: {
+        name: {
+            type: String as PropType<string>
         }
     },
-    setup:(props,context)=>{
-        const refKind=ref('支出')
-        const refPage=ref(0)
-        const refHasMore=ref(false)
-        onMounted(async ()=>{
-            const response =await http.get<Resources<Tag>>('/tags',{
-                kind:'expenses',
-                _mock:'tagIndex'
+    setup: (props, context) => {
+        const refKind = ref('支出')
+        const { tags: incomeTags,
+            hasMore: hasMore2,
+            fetchTags: fetchTags2
+        } = useTags((page) => {
+            return http.get<Resources<Tag>>('/tags', {
+                kind: 'income',
+                page: page + 1,
+                _mock: 'tagIndex'
             })
-            const {resources,pager}=response.data
-            refExpensesTags.value=resources
-            refHasMore.value=(pager.page-1) * pager.per_page + resources.length < pager.count
         })
-        const refExpensesTags=ref<Tag[]>([])
-        onMounted(async ()=>{
-            const response =await http.get<{resources:Tag[]}>('/tags',{
-                kind:'income',
-                _mock:'tagIndex'
-            })
-            refIncomeTags.value=response.data.resources
-        })
-        const refIncomeTags=ref<Tag[]>([])
-        return ()=>(
-            <MainLayout>{
-                {
-                    title:()=>'记一笔',
-                    icon:()=><img src={comeback} class={s.navIcon}/>,
-                    default:()=>
-                        <>
-                            <div class={s.wrapper}>
-                                <Tabs v-model:selected={refKind.value} class={s.tabs}>
-                                    <Tab name="支出" >
-                                        <div class={s.tags_wrapper}>
-                                        <div class={s.tag}>
-                                            <div class={s.sign}>
-                                                <img src={addBlack} class={s.createTag}/>
-                                            </div>
-                                            <div class={s.name}>
-                                                新增
-                                            </div>
-                                        </div>
-                                        {refExpensesTags.value.map(tag =>
-                                            <div class={[s.tag, s.selected]}>
-                                                <div class={s.sign}>
-                                                    {tag.sign}
-                                                </div>
-                                                <div class={s.name}>
-                                                    {tag.name}
-                                                </div>
-                                            </div>
-                                        )}
-                                        </div>
-                                        <div class={s.more}>
-                                            {refHasMore.value?
-                                                <Button>加载更多</Button>:<span>没有更多</span>}
-                                        </div>
-                                    </Tab>
-                                    <Tab name="收入" class={s.tags_wrapper}>
-                                        <div class={s.tag}>
-                                            <div class={s.sign}>
-                                                <img src={addBlack} class={s.createTag}/>
-                                            </div>
-                                            <div class={s.name}>
-                                                新增
-                                            </div>
-                                        </div>
-                                        {refIncomeTags.value.map(tag =>
-                                            <div class={[s.tag, s.selected]}>
-                                                <div class={s.sign}>
-                                                    {tag.sign}
-                                                </div>
-                                                <div class={s.name}>
-                                                    {tag.name}
-                                                </div>
-                                            </div>
-                                        )}
-                                    </Tab>
-                                </Tabs>
-                                <div class={s.inputPad_wrapper}>
-                                    <InputPad/>
-                                </div>
-                            </div>
-                        </>
-                }
-            }</MainLayout>
+        return () => (
+            <MainLayout class={s.layout}>{{
+                title: () => '记一笔',
+                icon: () => <img src={comeback} class={s.navIcon}/>,
+                default: () => <>
+                    <div class={s.wrapper}>
+                        <Tabs v-model:selected={refKind.value} class={s.tabs}>
+                            <Tab name="支出">
+                                <Tags kind="expenses"/>
+                            </Tab>
+                            <Tab name="收入">
+                                <Tags kind="income"/>
+                            </Tab>
+                        </Tabs>
+                        <div class={s.inputPad_wrapper}>
+                            <InputPad />
+                        </div>
+                    </div>
+                </>
+            }}</MainLayout>
         )
     }
 })
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -13,16 +13,16 @@ export const mockSession: Mock = (config) => {
 }
 
 export const mockTagIndex: Mock = (config) => {
-    const {kind,page} = config.params
-    const per_page=25
-    const count=26
+    const { kind, page } = config.params  //拿到响应的数据
+    const per_page = 25
+    const count = 26
     let id = 0
     const createId = () => {
         id += 1
         return id
     }
-    const createPaper=(page)=>({
-        page,per_page,count
+    const createPaper = () => ({
+        page, per_page, count
     })
     const createTag = (n = 1, attrs?: any) =>
         Array.from({ length: n }).map(() => ({
@@ -32,17 +32,18 @@ export const mockTagIndex: Mock = (config) => {
             kind: config.params.kind,
             ...attrs
         }))
-    const createBody=(n=1,attrs?:any)=>(
-        {resources: createTag(n) ,pager:createPaper(page)}
-    )
+    const createBody = (n = 1, attrs?: any) => ({
+        resources: createTag(n), pager: createPaper()
+    })
 
-    if (kind === 'expenses' && (page===1 || !page)) {
-        return [200,  createBody(25)]
-    } else if(kind === 'expenses' && page===2){
+    if (kind === 'expenses' && (!page || page === 1)) {
+        return [200, createBody(25)]
+    } else if (kind === 'expenses' && page === 2) {
         return [200, createBody(1)]
-    }
-    else {
-        return [200, { resources: createTag(20) }]
+    } else if (kind === 'income' && (!page || page === 1)) {
+        return [200, createBody(25)]
+    } else {
+        return [200, createBody(1)]
     }
 
 }
