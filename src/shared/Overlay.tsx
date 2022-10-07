2a238daf10adcdf7543ced4b2c93bc854cd1931e
DaoZhang1588 <1205308740@qq.com>
2022-10-07 16:45:55 +0800

    完成退出登录，显示用户邮箱

--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -1,11 +1,13 @@
-import {defineComponent, PropType, ref} from "vue";
+import {defineComponent, onMounted, PropType, ref} from "vue";
 import charts from "../assets/icons/charts.svg"
 import notice from "../assets/icons/notice.svg"
-import { RouterLink } from 'vue-router';
+import {RouterLink, useRoute} from 'vue-router';
 import classify from "../assets/icons/classify.svg"
 import box from "../assets/icons/box.svg"
 import s from "./Overlay.module.scss"
 import menu from "../assets/icons/menu.svg";
+import {mePromise} from "./me";
+import {Dialog} from "vant/es";
 
 export const Overlay =defineComponent({
     props:{
@@ -14,16 +16,40 @@ export const Overlay =defineComponent({
         }
     },
     setup:(props,context)=>{
-        const onClickSignIn = () => { }
         const close=()=> {
             props.onClose?.()
+        }
+        const route=useRoute()
+        const me=ref<User>()
+        onMounted( async ()=>{
+            const response=await mePromise
+            // @ts-ignore
+            me.value=response?.data.resource
+
+        })
+        const onSignOut=async ()=>{
+           await Dialog.confirm({
+                title:'确认',
+                message:'你真的要退出吗'
+            })
+            localStorage.removeItem('jwt')
         }
         return ()=><>
             <div class={s.mask} onClick={close}></div>
                 <div class={s.overlay}>
-                <section class={s.currentUser} onClick={onClickSignIn}>
-                    <h2>未登录用户</h2>
-                    <p>点击这里登录</p>
+                <section class={s.currentUser}>
+                    {me.value ?
+                        (<div>
+                            <h2 class={s.email}>{me.value?.email}</h2>
+                                <p onClick={onSignOut}>点击这里退出登录</p>
+                         </div>
+                        ) :
+                    <RouterLink to={`/sign_in?return_to=${route.fullPath}`}>
+                        <h2>未登录用户</h2>
+                        <p> 点击这里登录</p>
+                    </RouterLink>
+                    }
+
                 </section>
                     <nav>
                         <ul class={s.action_list}>
--- src/shared/me.tsx
+++ src/shared/me.tsx
@@ -1,14 +1,10 @@
 import { AxiosResponse } from "axios";
 import { http } from "./Http"
 
-export let mePromise: Promise<AxiosResponse<{
-    resource: {
-        id: number;
-    };
-}>> | undefined
+export let mePromise: Promise<AxiosResponse<Resources<User>>> | undefined
 
 export const refreshMe = () => {
-    mePromise = http.get<{ resource: { id: number } }>('/me')
+    mePromise = http.get<Resources<User>>('/me')
     return mePromise
 }
 
