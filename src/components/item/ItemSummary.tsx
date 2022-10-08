9c9184955e42f3cc9adc958e560e5328606ae220
DaoZhang1588 <1205308740@qq.com>
2022-10-08 21:49:50 +0800

    解决一个关于

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -79,15 +79,15 @@ export const ItemSummary=defineComponent({
                     <ul class={s.total}>
                         <li>
                             <span>收入</span>
-                            <span>128</span>
+                            <Money value={itemsBalance.income} />
                         </li>
                         <li>
                             <span>支出</span>
-                            <span>99</span>
+                            <Money value={itemsBalance.expenses} />
                         </li>
                         <li>
                             <span>净收入</span>
-                            <span>39</span>
+                            <Money value={itemsBalance.balance} />
                         </li>
                     </ul>
                     <ol class={s.list}>
